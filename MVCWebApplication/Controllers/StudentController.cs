a90a694b6c0946ee9330d7f57cb4160d5a9d3bd1
AMAR V. KAMBLE <kamarjit77@gmail.com>
2023-02-06 13:56:56 +0530

    Add project files.

---
+++ MVCWebApplication/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using MVCWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            ViewBag.Name = student.Name;
+            ViewBag.Mobile = student.Mobile;
+            ViewBag.Address = student.Address;
+            return View("~/Views/Message.cshtml");
+        }
+
+        [HttpPost]
+        public JsonResult GetStudentData(Student student)
+        {
+            student.Id = 1;
+
+            return Json(student);
+        }
+    }
+}
---
+++ MVCWebApplication/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [StringLength(20, ErrorMessage = "Name size is exceeding than 20.")]
+        public string Name { get; set; }
+
+        [MaxLength(12)]
+        public string Mobile { get; set; }
+
+        [StringLength(15, ErrorMessage = "Address size is exceeding than 15.")]
+        public string Address { get; set; }
+    }
+}
